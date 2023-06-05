8d67dbb7b8ae007979719a3632308d0a5b386c7a
zicku <2108096004@student.walisongo.ac.id>
2023-06-06 04:31:09 +0700

    update delate foto

--- penjual/deleteProduk.php
+++ penjual/deleteProduk.php
@@ -7,10 +7,27 @@ $id = $_GET['id'];
 $result = mysqli_query($connection, "DELETE FROM produk WHERE id='$id'");
 
 if (mysqli_affected_rows($connection) > 0) {
-  $_SESSION['info'] = [
-    'status' => 'success',
-    'message' => 'Berhasil menghapus data'
-  ];
+  // Kode untuk menghapus gambar terkait
+  $selectQuery = "SELECT * FROM produk WHERE id='$id'";
+  $selectResult = mysqli_query($connection, $selectQuery);
+  $row = mysqli_fetch_array($selectResult);
+  $imagePath = "gambar" . $row['foto'];
+  if (file_exists($imagePath)) {
+    unlink($imagePath);
+  }
+
+  if (unlink($imagePath)) {
+    $_SESSION['info'] = [
+      'status' => 'success',
+      'message' => 'Data berhasil dihapus beserta gambar'
+    ];
+  } else {
+    $_SESSION['info'] = [
+      'status' => 'success',
+      'message' => 'Data berhasil dihapus, tetapi gagal menghapus gambar'
+    ];
+  }
+
   header('Location: ./index.php');
 } else {
   $_SESSION['info'] = [
@@ -19,3 +36,4 @@ if (mysqli_affected_rows($connection) > 0) {
   ];
   header('Location: ./index.php');
 }
+?>
