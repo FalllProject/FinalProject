65c1a2dda5a4dc1cbe9b50be95162c59d4a44d03
zicku <2108096004@student.walisongo.ac.id>
2023-06-07 02:28:58 +0700

    update store
    udah bisa kehubung

--- penjual/storeProduk.php
+++ penjual/storeProduk.php
@@ -12,7 +12,7 @@ if ($namaBarang == "") {
     $tmp_dir = $_FILES['foto']['tmp_name'];
     $ukuran = $_FILES['foto']['size'];
 
-    $direktori = 'gambar'; // Path tempat penyimpanan gambar
+    $direktori = 'gambar/'; // Path tempat penyimpanan gambar
     $ekstensi = strtolower(pathinfo($file, PATHINFO_EXTENSION)); // Dapatkan informasi ekstensi gambar
     $valid_ekstensi = array('jpeg', 'jpg', 'png', 'gif'); // Ekstensi yang diperbolehkan
     $gambar = rand(1000, 1000000) . "." . $ekstensi;
@@ -25,11 +25,11 @@ if ($namaBarang == "") {
             move_uploaded_file($tmp_dir, $direktori . $gambar);
 
             // Simpan data ke database
-            $query = mysqli_query($connection, "INSERT INTO produk (id, namaBarang, harga, deskripsi, gambar) VALUES (NULL, '$namaBarang', '$harga', '$deskripsi', '$gambar')");
+            $query = mysqli_query($connection, "INSERT INTO produk (id, namaBarang, harga, deskripsi, foto) VALUES (NULL, '$namaBarang', '$harga', '$deskripsi', '$gambar')");
             if ($query) {
                 echo "Berhasil disimpan<br/>";
                 echo "Nama: $namaBarang <br/> <img src='$direktori$gambar' height='200'><br/>";
-                echo "Berhasil disimpan.<br/><a href='tampil_foto.php'>Lihat Halaman Berikutnya</a>";
+                echo "Berhasil disimpan.<br/><a href='index.php'>Lihat Halaman Berikutnya</a>";
             } else {
                 echo "Gagal menyimpan: " . mysqli_error($connection);
             }
