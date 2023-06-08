f4df4f998287dd9d44a65deced16ffb1587004f0
zicku <2108096004@student.walisongo.ac.id>
2023-06-08 18:23:12 +0700

    Revert "Update index.php"
    This reverts commit c2bcd9f5be07d9bb22a202d14ec4ef5a07d0e87f.

--- penjual/index.php
+++ penjual/index.php
@@ -12,14 +12,14 @@ $result = mysqli_query($connection, "SELECT * FROM produk");
   <a href="./createProduk.php" class="btn btn-primary">Tambah Data</a>
   </div>
 
-<div class="row gx-5">
+<div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
     <?php
     while ($data = mysqli_fetch_array($result)) :
     ?>
-    <div class="col-3">
+    <div class="col">
         <div class="card">
         
-        <?php echo "<img src='gambar/$data[foto]' width=237 height=240/>";?>
+        <?php echo "<img src='gambar/$data[foto]'width=250 height=250/>";?>
 
         <div class="card-body">
             <h5 class="card-title"><?= $data['namaBarang'] ?></h5>
