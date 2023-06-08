a5c6ecb8091bb75f19f7092fd18297e6d3687b9a
zicku <2108096004@student.walisongo.ac.id>
2023-06-08 18:04:10 +0700

    update produk
    benerin update produk

--- penjual/updateProduk.php
+++ penjual/updateProduk.php
@@ -6,8 +6,27 @@ $id = $_POST['id'];
 $namaBarang = $_POST['namaBarang'];
 $harga = $_POST['harga'];
 $deskripsi = $_POST['deskripsi'];
+$foto= $_FILES['foto']['name'];
+$foto_tmp=$_FILES['']['foto_tmp'];
+$profile_location='gambar/'.$foto;
 
-$query = mysqli_query($connection, "UPDATE produk SET id = '$id', namaBarang = '$namaBarang', deskripsi = '$deskripsi' WHERE id = '$id'");
+
+if($foto != ' '){
+
+  $query = mysqli_query($connection, "UPDATE produk SET id = '$id', namaBarang = '$namaBarang', deskripsi = '$deskripsi', foto='$foto' WHERE id = '$id'");
+  $update_query= mysqli_query($connection,$query);
+
+  if ( $update_query){
+    move_update_file($foto_tmp,$profile_location);
+    header("location:./index.php")
+
+  }
+}else {
+  echo " data gagal update ";
+}
+
+
+
 if ($query) {
   $_SESSION['info'] = [
     'status' => 'success',
